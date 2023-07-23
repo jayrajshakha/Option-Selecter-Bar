ab83af845951c3209d104a53a4ce2e99cf902c2a
jay <jayrajshakha01@gmail.com>
2023-07-23 20:17:57 +0530

    working

---
+++ main.js
@@ -0,0 +1,6 @@
+
+var select = document.getElementsByClassName('select');
+var selectText = document.getElementById('selectText');
+var options = document.querySelectorAll(".option");
+
+console.log(options)
