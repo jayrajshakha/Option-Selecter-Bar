50ff39be561a61fa099c07392eb2d32d43735eed
jay <jayrajshakha01@gmail.com>
2023-07-24 16:22:59 +0530

    Complete

--- main.js
+++ main.js
@@ -4,15 +4,17 @@ const selectText = document.querySelector("#selectText");
 const list = document.querySelector("#list");
 const options = document.querySelectorAll(".option");
 
-     select.onclick = function(){
-    list.classList.toggle("hide ")
-     }
+select.onclick = function(){
+    list.classList.toggle("hide");
+}
+
 
 for(option of options){
     option.addEventListener(
         "click",
         function(){
             selectText.innerHTML = this.textContent;
+            list.classList.toggle("hide");
 
             
         }
